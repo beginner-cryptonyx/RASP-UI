a184cd4075472722aefec7d07b229c901912d624
beginner-cryptonyx <mayanksethi242008@gmail.com>
2026-09-18 00:03:33 +0530

    Statprops added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import CTA from "./Registy/Marketing/CTA";
+import Hero from "./Registy/Marketing/Hero";
+import Statistic from "./Registy/Content/Statistic";
+import { statistics } from "./DummyData";
+
 function App() {
- return (<div>
-    <h1>This Could be used as a playground for testing</h1>
- </div>)
+  return (
+    <div>
+      <h1>This Could be used as a playground for testing</h1>
+      <Hero></Hero>
+      <CTA></CTA>
+      <div className="grid grid-cols-3">
+        {statistics.map((stat) => (
+          <Statistic {...stat}></Statistic>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
