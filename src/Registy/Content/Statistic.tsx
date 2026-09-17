67787e8644553f83fc90fecf4b5bd7eb56b6881c
beginner-cryptonyx <mayanksethi242008@gmail.com>
2026-09-18 00:03:21 +0530

    Made basic stat component

--- src/Registy/Content/Statistic.tsx
+++ src/Registy/Content/Statistic.tsx
@@ -1,7 +1,13 @@
-export default function Statistic() {
+import { type StatisticProps } from "../../Lib/types"
+
+export default function Statistic({value, suffix, label}: StatisticProps) {
     return (
-        <div>
-            Statistic
+        <div className="flex flex-col gap-1">
+            <div className="flex items-baseline gap-1">
+                <span className="text-4xl font-bold text-text">{value}</span>
+                <span className="text-xl font-bold mb-auto mt-0.5 text-accent">{suffix}</span>
+            </div> 
+            <span className="tesx-sm text-secondary">{label}</span>
         </div>
     )
 }
