2d5be90f4a3a1d53c8e0da4efd45a08996016344
beginner-cryptonyx <mayanksethi242008@gmail.com>
2026-09-20 19:51:38 +0530

    Minor adjustments, deleted unimporant files, formatting changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import CTA from "./Registy/Marketing/CTA";
-import Hero from "./Registy/Marketing/Hero";
 import Statistic from "./Registy/Content/Statistic";
 import { statistics } from "./DummyData";
 import useTheme from "./Theme/UseTheme";
@@ -9,7 +8,6 @@ function App() {
   return (
     <div>
       <h1>This Could be used as a playground for testing</h1>
-      <Hero></Hero>
       <CTA></CTA>
       <div className="grid grid-cols-3 ">
         {statistics.map((stat) => (
@@ -17,6 +15,7 @@ function App() {
         ))}
       </div>
       <button onClick={() => {themeSystem.setColorScheme("Coffee")}}>Koffee</button>
+      <button onClick={() => {themeSystem.toggleMode()}}>Toggle Modeee</button>
     </div>
   );
 }
