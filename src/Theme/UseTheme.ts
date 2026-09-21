a38caed837915db3fb31d7f650c5a3417a2eda7f
beginner-cryptonyx <mayanksethi242008@gmail.com>
2026-09-21 18:59:44 +0530

    fixed minor bug in useTheme where localstorage theme was not being used

--- src/Theme/UseTheme.ts
+++ src/Theme/UseTheme.ts
@@ -2,51 +2,55 @@ import { type ThemeName, type Mode } from "./theme";
 import { Themes } from "./theme";
 import { useState, useEffect } from "react";
 
-
 const MODE_KEY = "PROJECTNAME-MODE";
 const SCHEME_KEY = "PROJECTNAME-COLORSCHEME";
 const DEFAULT_SCHEME: ThemeName = "Crimson";
 
-
-function isMode(value: unknown){
-  if (value === "light" || value ==="dark"){
-    return true
+function isMode(value: unknown) {
+  if (value === "light" || value === "dark") {
+    return true;
   }
-  return false
+  return false;
 }
 
-function isThemeName(value: unknown){
-  if (typeof value === "string" && value in Themes){
-    return true
+function isThemeName(value: unknown) {
+  if (typeof value === "string" && Themes.includes(value)) {
+    return true;
   }
-  return false
+  return false;
 }
 
 export default function useTheme() {
   const [mode, setMode] = useState<Mode>(() => {
     const savedMode = localStorage.getItem(MODE_KEY);
-    if (isMode(savedMode)){ return savedMode as Mode}
+    if (isMode(savedMode)) {
+      return savedMode as Mode;
+    }
     return window.matchMedia("(prefers-color-scheme: dark)").matches
-        ? "dark"
-        : "light";
+      ? "dark"
+      : "light";
   });
 
   const [colorScheme, setColorScheme] = useState<ThemeName>(() => {
     const savedColorScheme = localStorage.getItem(SCHEME_KEY);
-    if (isThemeName(savedColorScheme)){return savedColorScheme as ThemeName}
-    return DEFAULT_SCHEME;
+    if (isThemeName(savedColorScheme)) {
+      console.log(savedColorScheme)
+      return savedColorScheme as ThemeName;
+    } else {
+      return DEFAULT_SCHEME;
+    }
   });
 
   const toggleMode = () => {
-    setMode((prevMode) => (prevMode === "light" ? "dark":"light"))
-  }
+    setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+  };
 
   useEffect(() => {
-    document.documentElement.dataset.theme = colorScheme
-    document.documentElement.dataset.mode = mode
-    localStorage.setItem(MODE_KEY, mode)
-    localStorage.setItem(SCHEME_KEY, colorScheme)
+    document.documentElement.dataset.theme = colorScheme;
+    document.documentElement.dataset.mode = mode;
+    localStorage.setItem(MODE_KEY, mode);
+    localStorage.setItem(SCHEME_KEY, colorScheme);
   }, [colorScheme, mode]);
 
-  return {setMode, setColorScheme, mode, colorScheme, toggleMode}
+  return { setMode, setColorScheme, mode, colorScheme, toggleMode };
 }
